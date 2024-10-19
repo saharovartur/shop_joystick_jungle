5c93a973dc219a9516e709b62239c4df5526e138
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-19 18:13:35 +0400

    добавил имя приложения

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from shop import views
 
+app_name = 'shop'
+
 urlpatterns = [
     path('', views.product_list, name='post_list'),
     path('<slug:category_slug>/', views.product_list, name='post_list_by_category'),
