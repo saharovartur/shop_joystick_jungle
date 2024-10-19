bd0c3e4a68175d1d1dadcacfe5028338def8edf2
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-19 17:07:09 +0400

    написал вью для списка товаров с фильтрацией по категориям

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from shop.models import Category, Product
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category,
+                                     slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'prodcuts': products})
+
+
+
