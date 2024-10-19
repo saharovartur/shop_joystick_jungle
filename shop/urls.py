f1da90a7513109b4136942e021b02a37fd23dae4
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-19 17:52:27 +0400

    добавил urls для представлений post_list, post_detail

---
+++ shop/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from shop import views
+
+urlpatterns = [
+    path('', views.product_list, name='post_list'),
+    path('<slug:category_slug>/', views.product_list, name='post_list_by_category'),
+    path('<int:id/<slug:slug>/', views.product_detail, name='product_detail'),
+]
