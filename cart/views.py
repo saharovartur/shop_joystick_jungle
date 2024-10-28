406c7bf86fda0a0e4979f74def43c962f116f0c1
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-28 20:09:44 +0400

    добавил имя шаблонов url в return redirect view

--- cart/views.py
+++ cart/views.py
@@ -23,7 +23,7 @@ def cart_add(request, product_id):
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'],
                  override_quantity=cd['override_quantity'])
-    return redirect('cart_detail')
+    return redirect('cart:cart_detail')
 
 
 @require_POST
@@ -34,5 +34,5 @@ def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart_detail')
+    return redirect('cart:cart_detail')
 
