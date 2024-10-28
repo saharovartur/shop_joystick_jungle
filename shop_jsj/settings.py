5a5d3174d5a3514f6d94f105e03454eb222b05a9
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-28 11:07:14 +0400

    добавил ключ для хранения корзины в сеансе

--- shop_jsj/settings.py
+++ shop_jsj/settings.py
@@ -81,8 +81,6 @@ DATABASES = {
     }
 }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -126,5 +124,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ключ для хранения состояния корзины в сеансе
 
 CART_SESSION_ID = 'cart'
