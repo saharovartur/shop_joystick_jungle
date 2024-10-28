4bfbce5146e34cb27e7410b9a72ccdb64911f568
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-28 19:55:34 +0400

    добавил два представления для возможность добавить и удалить товар из корзины

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from cart.cart import Cart
+from cart.forms import CartAddProductForm
+from shop.models import Product
+
+
+@require_POST
+def cart_add(request, product_id):
+    """
+    Добавление товара в корзину
+    """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'],
+                 override_quantity=cd['override_quantity'])
+    return redirect('cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    """
+    Удаление товаров из корзины
+    """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart_detail')
+
