4bdc1f964b4a05f5b7a33f9a0a0f674efb7b1c9a
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-29 15:52:08 +0400

    перенес строчку с "подключением" приложения на вторую строку для корректной работы

--- shop_jsj/urls.py
+++ shop_jsj/urls.py
@@ -5,11 +5,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cart.urls', namespace='cart')),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT )
+                          document_root=settings.MEDIA_ROOT)
