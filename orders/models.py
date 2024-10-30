8ad1e608a6f7a514eaffc777ba24c69d11ad6b82
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-30 22:16:06 +0400

    добавил модель для детальной инфо о заказе

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from shop.models import Product
+
 
 class Order(models.Model):
     """Модель заказа"""
@@ -37,3 +39,24 @@ class Order(models.Model):
         return sum(item.get_cost() for item in self.items.all())
 
 
+class OrderItem(models.Model):
+    """Модель для детальной инфо о заказе"""
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE,
+                              verbose_name='Заказ')
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE,
+                                verbose_name='Товар')
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2,
+                                verbose_name='Цена')
+    quantity = models.PositiveIntegerField(default=1,
+                                           verbose_name='Количество')
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
