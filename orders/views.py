93cad3bee78b4148209cf5dab65f297efd99f226
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-31 15:01:23 +0400

    добавил вью для регистрации заказа

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from orders.forms import OrderCreateForm
+from orders.models import OrderItem
+
+
+def order_create(request):
+    """
+    Вью создания заказа
+    """
+    cart = Cart(request)
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item["product"],
+                    price=item["price"],
+                    quantity=item["quantity"],
+                )
+
+            cart.clear()
+            return render(request, "order/created.html", {"order": order})
+    else:
+        form = OrderCreateForm()
+    return render(request, "order/created.html", {"cart": cart, "form": form})
