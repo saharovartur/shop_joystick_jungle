6c232d616dc15f4e1e6dc6e157a6eaa305cf303b
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-10-31 18:43:05 +0400

    отформотировал файл с помощью black

--- shop_jsj/urls.py
+++ shop_jsj/urls.py
@@ -4,12 +4,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('cart/', include('cart.urls', namespace='cart')),
-    path('', include('shop.urls', namespace='shop')),
-
+    path("admin/", admin.site.urls),
+    path("cart/", include("cart.urls", namespace="cart")),
+    path("orders/", include("orders.urls", namespace="orders")),
+    path("", include("shop.urls", namespace="shop")),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
