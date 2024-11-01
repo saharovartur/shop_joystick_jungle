2438f0e59b213cccad6e7219b743de42da644a42
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-01 11:01:38 +0400

    добавил функцию для вызова задания celery после оформления заказа

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from orders.services.tasks import order_created
 
 
 def order_create(request):
@@ -23,6 +24,7 @@ def order_create(request):
                 )
 
             cart.clear()
+            order_created.delay(order.id)
             return render(request, "order/created.html", {"order": order})
     else:
         form = OrderCreateForm()
