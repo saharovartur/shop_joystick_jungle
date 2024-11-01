7d45c6f05f5bc0a5ff403c4467906009da4c46e8
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-01 10:30:08 +0400

    исправил ошибки в нейминге

--- shop_jsj/celery.py
+++ shop_jsj/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 # Настройки Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.jsj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop_jsj.settings")
 app = Celery("shop_jsj")
-app.config_from_object("django.conf:settgins", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
