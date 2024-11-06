cb8e9cbee5352decb1028139b112000952aee9af
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-06 11:36:11 +0400

    добавил функцию для преобразования моделей в csv формат

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,9 @@
+import csv
+import datetime
+
 from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.html import mark_safe
 
 from orders.models import OrderItem, Order
 
@@ -6,6 +11,39 @@ from orders.models import OrderItem, Order
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
+
+
+def export_to_csv(modeladmin, request, queryset):
+    """
+    Функция экспорта моделей в CSV
+    """
+    opts = modeladmin.model._meta
+    content_disposition = f"attachment; filename={opts.verbose_name}.csv"
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = content_disposition
+    writer = csv.writer(response)
+    fields = [
+        field
+        for field in opts.get_fields()
+        if not field.many_to_many and not field.one_to_many
+    ]
+
+    # Записать первую строку с информацией заголовка
+    writer.writerow([field.verbose_name for field in fields])
+
+    # Записать строки данных
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime("%d/%m/%Y")
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = "Export to CSV"
 
 
 @admin.register(Order)
@@ -19,8 +57,26 @@ class OrderAdmin(admin.ModelAdmin):
         "postal_code",
         "city",
         "paid",
+        "order_payment",
         "created",
         "updated",
     ]
     list_filter = ["paid", "created", "updated"]
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
+
+    def order_payment(self, obj=None):
+        """
+        Метод вернет ссылку на сайт stripe с
+        инфой о платеже
+        :param obj: Order
+        :return: str
+        """
+        if obj:
+            url = obj.get_stripe_url()
+            if obj.stripe_id:
+                html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+                return mark_safe(html)
+        return ""
+
+    order_payment.short_description = "Инфо о платеже"
