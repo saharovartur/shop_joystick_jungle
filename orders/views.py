b31995ffffac94cded28a95df1c3d93ca2d9e770
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-06 20:44:25 +0400

    добавил функцию лля формирования pdf файла с деталми заказа из файла html

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,9 @@
-import weasyprint
-from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.template.loader import render_to_string
 from django.urls import reverse
+from xhtml2pdf import pisa
 
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
@@ -50,7 +49,8 @@ def admin_order_pdf(request, order_id):
     html = render_to_string("order/pdf.html", {"order": order})
     response = HttpResponse(content_type="application/pdf")
     response["Content-Disposition"] = f"filename=order_{order.id}.pdf"
-    weasyprint.HTML(string=html).write_pdf(
-        response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / "css/pdf.css")]
-    )
+    pisa_status = pisa.CreatePDF(
+        html, response
+    )  # stylesheets=[(settings.STATIC_ROOT / "css/pdf.css")]
+
     return response
