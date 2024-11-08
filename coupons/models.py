22aeb4669c183ed1b1fba8b3a4aa04949a4a2ec9
Artur Sakharov <108172893+saharovartur@users.noreply.github.com>
2024-11-08 11:35:32 +0400

    добавил новое приложение для работы с промо-кодами на скидку / добавил модель для хранения промо-кодов

---
+++ coupons/models.py
@@ -0,0 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Coupon(models.Model):
+    """
+    Модель для хранения промо-кодов
+    """
+
+    code = models.CharField(max_length=50, unique=True, verbose_name="Код")
+    valid_form = models.DateTimeField(verbose_name="От")
+    valid_to = models.DateTimeField(verbose_name="Годен до")
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        help_text="Процентное значение (от 0 до 100)",
+        verbose_name="Скидка",
+    )
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.code
